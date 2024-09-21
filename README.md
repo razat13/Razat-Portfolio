57bb59be0b8e03b7429b63eb6c52fffbb16ec12a
razat13 <tanishqmahaur@gmail.com>
2024-09-21 14:11:29 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Angular Portfolio App
+
+This is my personal portfolio website built with Angular, Typescript, HTML5, CSS3, Bootstrap, Ngx-Bootstrap, DevIcons and Angular Material. It's currently deployed on the web.
+
+## The Project
+
+This portfolio is a **dynamic and responsive web application** built using **Angular**, showcasing my skills and projects as a software developer. The design and development of this portfolio aimed to provide a seamless user experience across different devices while highlighting various aspects of my professional journey. 
+
+The potfolio has sections of home, about, education, portfolio projects and contact information. Key features are its dynamic content, the responsive design and modern aesthetics.
+
+## Technologies Used
+
+- AngularCLI
+- Angular
+- Angular Materials
+- Typescript
+- HTML5
+- CSS3
+- Bootstrap
+- Ngx-Bootstrap
+- Dev Icons
+- Vercel
+- Vite
+
+## Development server
+
+You can run this project locally too.
+
+Clone this repo.
+
+Run `ng serve` for a dev server. 
+
+Navigate to `http://localhost:4200/`. 
+
+The application will automatically reload if you change any of the source files.
+
+## Code scaffolding
+
+Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+
+## Build
+
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { CardComponent } from '../card/card.component';
+import { Card } from '../_models/card';
+import { Tag } from '../_models/tags';
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [CardComponent],
+  templateUrl: './portfolio.component.html',
+  styleUrl: './portfolio.component.css',
+  providers: []
+})
+export class PortfolioComponent {
+
+  projects: Card[] = [
+    {
+      id: 1,
+      name: 'Expense Manager',
+      summary: 'The project developed using the MERN stack is an expense management system that helps individuals track and manage their expenses. The project includes features like User Registration and Authentication, Expense Input, Expense Categorization, Expense List, Graphical Representation and Expense Analytics',
+      description: '',
+      projectLink: 'https://github.com/razat13/ExpenseManager',
+      tags: [Tag.NODEJS, Tag.REACTJS, Tag.EXPRESS, Tag.MONGODB, Tag.HTML5, Tag.CSS3, Tag.JAVASCRIPT],
+      pictures: ["../../assets/casino01.png", "../../assets/casino02.png"]
+    },
+    {
+      id: 2,
+      name: 'Movie Overview',
+      summary: 'React-Based Entertainment Application.Where users can see an overview of 20 popular movies, top-rated movies, and upcoming movies.',
+      projectLink: 'https://github.com/razat13/Movie-Overview',
+      tags: [Tag.REACTJS,Tag.HTML5, Tag.CSS3, Tag.JAVASCRIPT],
+      pictures: ["../../assets/pokedex_01.png", "../../assets/pokedex_02.png", "../../assets/pokedex_03.png"],
+      description: ''
+    },
+    {
+      id: 3,
+      name: 'FakeShop Online',
+      summary: 'FakeShop is an online shopping platform where users can browse through a variety of products, view product details, add products to their cart, and proceed to checkout. This project is built using React.js and interacts with the FakeStoreAPI to fetch product data.',
+      description: '',
+      projectLink: 'https://github.com/razat13/MakerStudioProject',
+      tags: [Tag.REACTJS,Tag.JAVASCRIPT, Tag.HTML5, Tag.CSS3],
+      pictures: ["../../assets/teamtrack_06.png", "../../assets/teamtrack_07.png", "../../assets/teamtrack_08.png"]
+    },
+  ]
+
+
+  constructor(private titleService: Title) {
+    this.titleService.setTitle('Razat Portfolio')
+  }
+
+  trackById(index: number, project: Card): number {
+    return project.id;
+  }
+
+}
